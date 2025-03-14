97a7be95e9cdcc93db15bbcadb8ffcf879f83ccb
Globaltune <202308378@post.au.dk>
2025-03-14 11:31:57 +0100

    update to be able to run "dotnet run" and use link instead of run and debug

--- WebsiteCreation/WebsiteCreation/Program.cs
+++ WebsiteCreation/WebsiteCreation/Program.cs
@@ -8,20 +8,7 @@ var app = builder.Build();
 app.UseStaticFiles();
 app.MapFallbackToFile("html/index.html");
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
-app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthorization();
-
-//app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
